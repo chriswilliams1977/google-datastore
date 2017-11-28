e7fdeada374425467de84c09ba006721312dc46a
Pim Hoogendoorn <pim.hoogendoorn@willenium.nl>
2017-11-28 21:01:48 +0100

    🗑 Removed debounce

--- web/javascript/bookshelf.js
+++ web/javascript/bookshelf.js
@@ -4,31 +4,12 @@ var Bookshelf = (function () {
     const inputNode = document.querySelector('#search-phrase');
     const resultsNode = document.querySelector('#search-results');
 
-    // Returns a function, that, as long as it continues to be invoked, will not
-    // be triggered. The function will be called after it stops being called for
-    // N milliseconds. If `immediate` is passed, trigger the function on the
-    // leading edge, instead of the trailing.
-    var _debounce = function(func, wait, immediate) {
-    	var timeout;
-    	return function() {
-    		var context = this, args = arguments;
-    		var later = function() {
-    			timeout = null;
-    			if (!immediate) func.apply(context, args);
-    		};
-    		var callNow = immediate && !timeout;
-    		clearTimeout(timeout);
-    		timeout = setTimeout(later, wait);
-    		if (callNow) func.apply(context, args);
-    	};
-    };
-
     // Only search when user stops typing
-    var _onKeydown = _debounce(function(event) {
+    var _onKeydown = function(event) {
         if (!event) return;
         if (inputNode.value == '') return;
         _getBooksFromServer(event);
-    }, 500);
+    };
 
     var _getBooksFromServer = function(event) {
         var textdata = event.target.value;
